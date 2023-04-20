852627bf01aa8050f2049eda8b234296f97cbfd6
Rhythm1821 <alamrawat1821@gmail.com>
2023-04-20 17:05:21 +0530

    Project setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='Delivery Time Estimator',
+    version='0.0.1',
+    author='Rhythm1821',
+    author_email='alamrawat1821@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
